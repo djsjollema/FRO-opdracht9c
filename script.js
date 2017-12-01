1fd82f3b09a36db4dbd4aa68b3047b2800203b1a
Jelle Sjollema <djsjollema@gmail.com>
2017-12-01 16:31:49 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const myImage = document.getElementById('myImage');
+const thumbnailHolder = document.getElementById('thumbnailHolder');
+
+var image_names = ["QCbDf2n - Imgur.jpg",
+  "qHiwH4P - Imgur.jpg",
+  "redcliff-1920x1080.jpg",
+  "RedwineV2 1920x1200.jpg",
+  "road_1920_1200.jpg",
+  "Rojis.jpg",
+  "Ru2YZ68 - Imgur.jpg",
+  "t3_6t89rv.jpg",
+  "Winter pack (2).jpg"
+];
